a037642ac8adf65bf7e188d57c3a415559226930
otchi012 <hiroshi012931@gmail.com>
2020-10-13 12:00:20 +0000

    [Update]4-3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_13_091553) do
+ActiveRecord::Schema.define(version: 2020_10_13_113826) do
+
+  create_table "post_images", force: :cascade do |t|
+    t.text "shop_name"
+    t.integer "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
